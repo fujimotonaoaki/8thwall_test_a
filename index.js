d2b472275c71534df45cff0f6cabcad8872ffa5d
naoqilin <naoqilin@gmail.com>
2020-01-21 01:23:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -94,12 +94,12 @@ const imageTargetPipelineModule = () => {
 const onxrloaded = () => {
   // If your app only interacts with image targets and not the world, disabling world tracking can
   // improve speed.
-  XR8.xrController().configure({disableWorldTracking: true})
+  //XR8.xrController().configure({disableWorldTracking: true})
   XR8.addCameraPipelineModules([  // Add camera pipeline modules.
     // Existing pipeline modules.
     XR8.GlTextureRenderer.pipelineModule(),      // Draws the camera feed.
     XR8.Threejs.pipelineModule(),                // Creates a ThreeJS AR Scene.
-    XR8.XrController.pipelineModule(),           // Enables SLAM tracking.
+    //XR8.XrController.pipelineModule(),           // Enables SLAM tracking.
     XRExtras.AlmostThere.pipelineModule(),       // Detects unsupported browsers and gives hints.
     XRExtras.FullWindowCanvas.pipelineModule(),  // Modifies the canvas to fill the window.
     XRExtras.Loading.pipelineModule(),           // Manages the loading screen on startup.
